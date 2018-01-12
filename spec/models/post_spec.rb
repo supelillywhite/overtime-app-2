05ca19d4e80f25edb9c38f97f3a22cf688ba5481
Jerel Lillywhite <supelillywhite@gmail.com>
2018-01-12 10:07:37 -0700

    Added hours requested to post model

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe Post, type: :model do
       expect(@post).to_not be_valid
     end
 
-    it "has an overtime_request greater than 0.0" do
+    it 'has an overtime_request greater than 0.0' do
       @post.overtime_request = 0.0
       expect(@post).to_not be_valid
     end
