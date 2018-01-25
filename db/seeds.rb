b6f451d81d9183c75892f0489f343a2edb63572c
Jerel Lillywhite <supelillywhite@gmail.com>
2018-01-25 14:34:19 -0700

    Built out rake task for sending sms messages and STI for employees

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,29 @@
-@user = User.create(email: "test@test.com", 
+@employee = Employee.create(email: "test@test.com", 
                     password: "asdfasdf", 
                     password_confirmation: "asdfasdf", 
                     first_name: "Jon", 
                     last_name: "Snow",
-                    phone: "4352362570")
+                    phone: "4356199154")
 
-puts "1 User created"
+puts "1 employee created"
 
 AdminUser.create(email: "admin@test.com", 
                 password: "asdfasdf", 
                 password_confirmation: "asdfasdf", 
                 first_name: "Admin", 
                 last_name: "Name",
-                phone: "4352362570")
+                phone: "4356199154")
 
-puts "1 Admin User created"
+puts "1 Admin user created"
 
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 6.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 13.days))
+AuditLog.create!(user_id: @employee.id, status: 0, start_date: (Date.today - 20.days))
 
 puts "3 audit logs have been created"
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content hippies hippies hippies hippies hippies hippies hippies hippies hippies", user_id: @user.id, overtime_request: 2.5)
+  Post.create!(date: Date.today, rationale: "#{post} rationale content hippies hippies hippies hippies hippies hippies hippies hippies hippies", user_id: @employee.id, overtime_request: 2.5)
 end
 
 puts "100 Posts have been created"
